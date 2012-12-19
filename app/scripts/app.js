cc52a6f134c659fe932382c5e2c90d0b42ace4a4
Peter Müller <munter@fumle.dk>
2012-12-19 23:38:30 +0100

    yeoman init angular-all

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var angularExampleApp = angular.module('angularExampleApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
